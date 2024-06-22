e6db0d852794f2d7c84767b63b13f7711db1da49
unicesng <eunice.sng.2022@scis.smu.edu.sg>
2024-06-22 11:14:10 +0800

    feat: Initialise React app & DaisyUI

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import './App.css'
+import Home from './pages/Home';
+
+function App() {
+
+  return (
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </Router>
+  )
+}
+
+export default App
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,7 @@
+const Home = () => {
+    return (
+        <h1 className="text-black">Hello World</h1>
+    )
+}
+
+export default Home
