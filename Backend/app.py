dcb533346767519e93c96a91a392107cf3bb2afe
Juay Kai Xun <juaykaixun@hotmail.com>
2024-06-22 12:05:02 +0800

    update

--- Backend/app.py
+++ Backend/app.py
@@ -1,6 +1,5 @@
 from flask import Flask
-from bus_timings import bus_timings_bp
-from train_timings import train_timings_bp
+from bus_tracking import bus_timings_bp
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -8,7 +7,6 @@ CORS(app)
 
 # Register blueprints
 app.register_blueprint(bus_timings_bp, url_prefix='/bus')
-app.register_blueprint(train_timings_bp, url_prefix='/train')
 
 if __name__ == '__main__':
     app.run()
