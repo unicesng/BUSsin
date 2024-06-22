ae5c7ccb8e293eefa82b35dfcb94dfb8e203f64f
Juay Kai Xun <juaykaixun@hotmail.com>
2024-06-22 11:21:58 +0800

    update

--- Backend/app.py
+++ Backend/app.py
@@ -1,18 +1,14 @@
-from flask import Flask, jsonify  # Import jsonify function
+from flask import Flask
+from bus_timings import bus_timings_bp
+from train_timings import train_timings_bp
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
-
-@app.route('/bus-timings/<bus_number>')
-def get_bus_timings(bus_number):
-    # Add logic here to retrieve the bus timings for the specified bus number
-    timings = {'bus_number': bus_number, 'timings': ['8:00 AM', '10:30 AM', '1:00 PM']}
-    return jsonify(timings)
+# Register blueprints
+app.register_blueprint(bus_timings_bp, url_prefix='/bus')
+app.register_blueprint(train_timings_bp, url_prefix='/train')
 
 if __name__ == '__main__':
     app.run()
