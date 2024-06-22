2654ad9780dfcc3c148d7a9cfdd139783e3593d5
unicesng <eunice.sng.2022@scis.smu.edu.sg>
2024-06-22 13:05:16 +0800

    feat: NavBar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,18 +2,21 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css'
 import Home from './pages/Home';
 import Navbar from './components/navbar';
+import { ChakraProvider } from '@chakra-ui/react'
 
 function App() {
 
   return (
-    <Router>
-      <div>
-        <Navbar/>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </div>
-    </Router>
+    <ChakraProvider>
+      <Router>
+        <div className='w-screen'>
+          <Navbar/>
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </div>
+      </Router>
+    </ChakraProvider>
   )
 }
 
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import BUSsinLogo from "../assets/BUSsinLogo.png";
+
+const Navbar = () => {
+  return (
+    <nav className="bg-black border-gray-200 w-full flex items-center">
+      <div className="flex items-center">
+        <img src={BUSsinLogo} alt="BUSsin Logo" width="100" />
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
